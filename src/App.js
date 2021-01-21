1a0aac4b0a4aee83733f3fba07c3717e62f0fbb9
Abdelrhman Saied <abdelrhman.s.farag@gmail.com>
2021-01-21 21:13:05 +0200

    last

--- src/App.js
+++ src/App.js
@@ -50,12 +50,15 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask (!showAddTask)}  showAdd={showAddTask}/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
-        "NO Tasks to show"
+        "NO Tasks to Show"
       )}
     </div>
   );
